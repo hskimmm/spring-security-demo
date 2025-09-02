dee6c7db71d3e329b0e3dd19a5b6af4e8ad2c878
Kim Hyeong Seop <fbhydfa@gmail.com>
2025-09-02 16:29:33 +0900

    feat: 로그아웃 기능 추가

--- src/main/java/org/spring/springsecuritydemo/controller/user/LoginController.java
+++ src/main/java/org/spring/springsecuritydemo/controller/user/LoginController.java
@@ -1,5 +1,10 @@
 package org.spring.springsecuritydemo.controller.user;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +14,20 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication(); //인증된 사용자 객체
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+            /*
+                세션 무효화
+                SecurityContext 비움
+                remember-me 쿠키 제거
+            */
+        }
+
+        return "redirect:/login";
     }
 }
