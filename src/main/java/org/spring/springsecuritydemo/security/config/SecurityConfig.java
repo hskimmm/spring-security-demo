7a9bf0dce572abdec915bc1b26be7c5f1cbdabc6
Kim Hyeong Seop <fbhydfa@gmail.com>
2025-09-02 15:24:34 +0900

    시큐리티 기본설정 코드 추가

--- src/main/java/org/spring/springsecuritydemo/security/config/SecurityConfig.java
+++ src/main/java/org/spring/springsecuritydemo/security/config/SecurityConfig.java
@@ -1,4 +1,32 @@
 package org.spring.springsecuritydemo.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/images/**", "/css/**", "/js/**", "/vendor/**", "/favicon.*", "/*/icon-*", "/.well-known/**").permitAll()
+                        .requestMatchers("/", "/login", "/signup").permitAll()
+                        .requestMatchers("/user").hasAnyAuthority("ROLE_USER")
+                        .requestMatchers("/manager").hasAnyAuthority("ROLE_manager")
+                        .requestMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                )
+        ;
+        return http.build();
+    }
 }
